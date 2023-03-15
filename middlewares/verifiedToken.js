f64af449dc0b15ef9fd25cd3d31b920323457d33
Francsy <francsyludens@gmail.com>
2023-03-15 23:47:26 +0100

    add/middleware to verify tookens from back in admin and employee routers

--- middlewares/verifiedToken.js
+++ middlewares/verifiedToken.js
@@ -1,26 +1,26 @@
-// const express = require('express');
-// const jwt = require('jsonwebtoken');
-// const jwt_key = process.env.JWT_KEY;
-// const Employee = require('../schemas/employees');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const jwt_key = process.env.JWT_KEY;
+const Usuarios = require('../schemas/usuarios');
+const adminProtector = express.Router();
+const userProtector = express.Router();
 
-// const adminProtector = express.Router();
-// const userProtector = express.Router();
-
-
-/* 
 adminProtector.use(async (req, res, next) => {
     const token = req.cookies['access-token'];
     if (!token) return res.json({ message: 'Token not provided' }); // No token
     try {
         const decoded = jwt.verify(token, jwt_key);
-        if ((decoded.exp * 1000) < Date.now()) return res.json({ msg: 'Expired token' }); // Token had expired
-    
-        // const userData = await users.getRole(decoded.email);
-        if (!userData || userData[0].logged_in !== true || userData[0].role !== "admin") return res.json({ msg: 'Invalid token' }); // Token verification failed
+        if ((decoded.exp * 1000) < Date.now()) return res.json({ message: 'Expired token' }); // Token had expired
+        let userData = await Usuarios.findOne({
+            attributes: ['email', 'rol'],
+            where: { email: decoded.email }
+        })
+        
+        if (!userData || userData.rol !== "admin") return res.json({ message: 'Invalid token' }); // Token verification failed
 
         const userForToken = {
             email: decoded.email,
-            id: decoded.id
+            rol: decoded.rol
         };
         const newToken = jwt.sign(userForToken, jwt_key, { expiresIn: '20m' }); // Renew the admin toker
         res.cookie('access-token', newToken, {
@@ -33,4 +33,39 @@ adminProtector.use(async (req, res, next) => {
         return res.json({ msg: 'Invalid token' });
     }
 });
- */
+
+
+userProtector.use(async (req, res, next) => {
+    const token = req.cookies['access-token'];
+    if (!token) return res.json({ message: 'Token not provided' }); // No token
+    try {
+        const decoded = jwt.verify(token, jwt_key);
+        if ((decoded.exp * 1000) < Date.now()) return res.json({ message: 'Expired token' }); // Token had expired
+        let userData = await Usuarios.findOne({
+            attributes: ['email', 'rol'],
+            where: { email: decoded.email }
+        })
+        
+        if (!userData || userData.rol !== "empleado") return res.json({ message: 'Invalid token' }); // Token verification failed
+
+        const userForToken = {
+            email: decoded.email,
+            rol: decoded.rol
+        };
+        const newToken = jwt.sign(userForToken, jwt_key, { expiresIn: '20m' }); // Renew the admin toker
+        res.cookie('access-token', newToken, {
+            httpOnly: true,
+            // secure: process.env.NODE_ENV === "production"
+        });
+        req.decoded = decoded;
+        next();
+    } catch (error) {
+        return res.json({ msg: 'Invalid token' });
+    }
+});
+
+
+module.exports = {
+    adminProtector,
+    userProtector
+}
