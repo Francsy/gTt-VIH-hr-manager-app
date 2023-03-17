198c40b4e8ece0709ffc61ee1b077e2098197387
Francsy <francsyludens@gmail.com>
2023-03-18 00:41:49 +0100

    add/icons to sidenavbars

--- client/src/components/AdminViews/AdminMain/AdminSideNavBar/AdminSideNavBar.jsx
+++ client/src/components/AdminViews/AdminMain/AdminSideNavBar/AdminSideNavBar.jsx
@@ -1,22 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
+import HomeIcon from "../../../../assets/home.svg"
+import UsersIcon from "../../../../assets/users.svg"
+import InboxIcon from "../../../../assets/inbox.svg"
+import LayersIcon from "../../../../assets/layers.svg"
+import CalendarIcon from "../../../../assets/calendar.svg"
+import CoinsIcon from "../../../../assets/coins.svg"
+import ClockIcon from "../../../../assets/clock.svg"
+import EditIcon from "../../../../assets/edit.svg"
+import ChecksIcon from "../../../../assets/checks.svg"
+import CheckIcon from "../../../../assets/check.svg"
+import PagesIcon from "../../../../assets/pages.svg"
+import SmileIcon from "../../../../assets/smile.svg"
+import LogOutIcon from "../../../../assets/log-out.svg"
+
+
+
 const AdminSideNavBar = () => {
   return <nav>
-    <Link to="/admin">&nbsp;Inicio</Link>
-    <Link to="/admin/personal">&nbsp;Personal</Link>
-    <Link to="/admin">&nbsp;Solicitudes</Link>
-    <Link to="/admin">&nbsp;Gestiones</Link>
-    <Link to="/admin">&nbsp;Agenda</Link>
-    <Link to="/admin">&nbsp;Gastos</Link>
-    <Link to="/admin">&nbsp;Horarios</Link>
-    <Link to="/admin">&nbsp;Informes</Link>
-    <Link to="/admin">&nbsp;Permisos</Link>
-    <Link to="/admin">&nbsp;Tareas</Link>
-    <Link to="/admin">&nbsp;Nóminas</Link>
-    <Link to="/admin">&nbsp;Mi perfil</Link>
-    <Link to="/admin/select">&nbsp;Selección de sesión&nbsp;</Link>
-    <Link to="/" ><button>Cerrar sesión</button></Link>
+    <Link to="/admin"><img src={HomeIcon} alt="" />&nbsp;Inicio</Link>
+    <Link to="/admin/personal"><img src={UsersIcon} alt="" />&nbsp;Personal</Link>
+    <Link to="/admin"><img src={InboxIcon} alt="" />&nbsp;Solicitudes</Link>
+    <Link to="/admin"><img src={LayersIcon} alt="" />&nbsp;Gestiones</Link>
+    <Link to="/admin"><img src={CalendarIcon} alt="" />&nbsp;Agenda</Link>
+    <Link to="/admin"><img src={CoinsIcon} alt="" />&nbsp;Gastos</Link>
+    <Link to="/admin"><img src={ClockIcon} alt="" />&nbsp;Horarios</Link>
+    <Link to="/admin"><img src={EditIcon} alt="" />&nbsp;Informes</Link>
+    <Link to="/admin"><img src={ChecksIcon} alt="" />&nbsp;Permisos</Link>
+    <Link to="/admin"><img src={CheckIcon} alt="" />&nbsp;Tareas</Link>
+    <Link to="/admin"><img src={PagesIcon} alt="" />&nbsp;Nóminas</Link>
+    <Link to="/admin"><img src={SmileIcon} alt="" />&nbsp;Mi perfil</Link>
+    <Link to="/" ><img src={LogOutIcon} alt="" />&nbsp;Cerrar sesión</Link>
   </nav>;
 };
 
--- client/src/components/EmployeeViews/EmployeeMain/EmployeeSideNavBar/EmployeeSideNavBar.jsx
+++ client/src/components/EmployeeViews/EmployeeMain/EmployeeSideNavBar/EmployeeSideNavBar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom"
+
+import HomeIcon from "../../../../assets/home.svg"
+import InboxIcon from "../../../../assets/inbox.svg"
+import CalendarIcon from "../../../../assets/calendar.svg"
+import CoinsIcon from "../../../../assets/coins.svg"
+import CheckIcon from "../../../../assets/check.svg"
+import SmileIcon from "../../../../assets/smile.svg"
+import LogOutIcon from "../../../../assets/log-out.svg"
+
+
+
 const EmployeeSideNavBar = () => {
   return <nav>
-    <Link to="/user">&nbsp;Inicio</Link>
-    <Link to="/user">&nbsp;Solicitudes</Link>
-    <Link to="/user">&nbsp;Tareas</Link>
-    <Link to="/user">&nbsp;Agenda</Link>
-    <Link to="/user">&nbsp;Gastos&nbsp;</Link>
-    <Link to="/user">&nbsp;Mi perfil</Link>
-    <Link to="/" ><button>Cerrar sesión</button></Link>
+    <Link to="/user"><img src={HomeIcon} alt="" />&nbsp;Inicio</Link>
+    <Link to="/user"><img src={InboxIcon} alt="" />&nbsp;Solicitudes</Link>
+    <Link to="/user"><img src={CheckIcon} alt="" />&nbsp;Tareas</Link>
+    <Link to="/user"><img src={CalendarIcon} alt="" />&nbsp;Agenda</Link>
+    <Link to="/user"><img src={CoinsIcon} alt="" />&nbsp;Gastos&nbsp;</Link>
+    <Link to="/user"><img src={SmileIcon} alt="" />&nbsp;Mi perfil</Link>
+    <Link to="/" ><img src={LogOutIcon} alt="" />&nbsp;Cerrar sesión</Link>
 
   </nav>;
 };
