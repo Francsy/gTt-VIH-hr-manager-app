9a90bb7052be89f9cb13e638433dbf88be6c0e32
Manuel Murillo <mmpeira@gmail.com>
2023-03-18 20:04:10 +0100

    vista dashboardUser

--- client/src/components/EmployeeViews/EmployeeHeader/EmployeeHeader.jsx
+++ client/src/components/EmployeeViews/EmployeeHeader/EmployeeHeader.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import ProfileImg from "../../../assets/profile-employee.png"
+import ProfileImg from "../../../assets/profile-employee.png";
 const EmployeeHeader = () => {
-  return <header>
-    <div><button><img src={ProfileImg} alt="" />Nombre empleado</button></div>
-    </header>;
+  return (
+    <header>
+      <div className="header-employee">
+        <button>
+          <img src={ProfileImg} alt="" />
+          Nombre empleado
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default EmployeeHeader;
