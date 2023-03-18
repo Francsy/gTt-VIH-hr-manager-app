6ffa73f29fdad81e7d050ea02db4f38572df296e
Francsy <francsyludens@gmail.com>
2023-03-18 01:02:20 +0100

    fix/server old controller bug

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,6 @@ const express = require('express')
 const authController = require('../controllers/authController')
 const authRouter = express.Router()
 
-authRouter.post('/signup', authController.createNewEmployee);
 authRouter.post('/login', authController.authLogin)
 
 
